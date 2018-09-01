eb40045b8ef61c14574402882f4d0c94d4278178
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-09-01 19:54:39 +0530

    added router links to static pages and product listing pages

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from "@ngx-translate/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -9,13 +10,18 @@ import { TranslateService } from "@ngx-translate/core";
 export class FooterComponent implements OnInit {
 
   constructor(
-    private translate: TranslateService
+    private translate: TranslateService,
+    private router: Router
   ) {
     this.translate.setDefaultLang('en');
     this.translate.use('en');
   }
 
   ngOnInit() {
+  }
+
+  navigatePage(page) {
+    this.router.navigateByUrl(page);
   }
 
 }
