b589a090682003363d4a38e318e7d3914a65a908
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-09-03 13:00:50 +0530

    updated sub-header title , taking innput from each page

--- client/src/app/components/sub-header/sub-header.component.ts
+++ client/src/app/components/sub-header/sub-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sub-header',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub-header.component.scss']
 })
 export class SubHeaderComponent implements OnInit {
+
+  /**To get heading in the sub-header for each page */
+  @Input() title: string;
 
   constructor() { }
 
