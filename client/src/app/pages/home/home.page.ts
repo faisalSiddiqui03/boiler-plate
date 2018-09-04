5ac97f55a5e47d697c03d82d5679c32ac966783a
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-09-04 11:27:05 +0530

    added selection-wdget

--- client/src/app/pages/home/home.page.ts
+++ client/src/app/pages/home/home.page.ts
@@ -1,18 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { LifeCycle, Action, pwaLifeCycle, OnWidgetActionsLifecyle, OnWidgetLifecyle } from '@capillarytech/pwa-framework';
+import { LocationWidgetActions } from '@capillarytech/pwa-framework';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+
+@pwaLifeCycle()
+export class HomePage implements OnInit, OnWidgetLifecyle, OnWidgetActionsLifecyle {
 
   slideOpts = {
     effect: 'flip'
   };
   constructor() { }
+  orderMode = 'DELIVERY';
+  locationsWidgetAction = new EventEmitter();
+  dataLoaded: any = {};
+  isDropDownShown = false;
+  selectedCity = '';
 
   ngOnInit() {
+  }
+
+  widgetLoadingSuccess(name, data) {
+
+    console.log('home page -> location widget', name, data);
+    // let getAllCities = new Action(LocationWidgetActions.ACTION_FETCH_ALL_CITIES);
+
+    // this.locationsWidgetAction.emit(getAllCities);
+  }
+
+  handleWidgetLifecycle(x: LifeCycle) {
+  }
+
+  widgetActionFailed(name: string, data: any) {
+
+    console.log('failed name = ', name, ' data = ', data);
+  }
+
+  widgetActionSuccess(name: string, data: any) {
+
+    console.log('name = ', name, ' data = ', data );
+  }
+
+  widgetLoadingFailed(name: string, data: any) {
+
+    console.log('name = ', name, ' data = ', data );
+  }
+
+  widgetLoadingStarted(name: string, data: any) {
+
+    console.log('name = ', name, ' data = ', data );
+  }
+
+  /**
+   * @name: could be area, city town based on
+   * requirement of brand
+   */
+  toggleDropDown(name: string, force: boolean = false, forceValue?: boolean) {
+    this.isDropDownShown = true;
+  }
+
+  selectCity(cityName) {
+    this.selectedCity = cityName;
+    this.isDropDownShown = false;
   }
 
 }
