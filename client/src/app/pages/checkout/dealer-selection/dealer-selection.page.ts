16d89c3c8245084994ed94b37c0b65ab856826fd
anjicap <anjaneyulu.singavaram@capillarytech.com>
2018-09-10 13:03:54 +0530

    dealer selection changes

--- client/src/app/pages/checkout/dealer-selection/dealer-selection.page.ts
+++ client/src/app/pages/checkout/dealer-selection/dealer-selection.page.ts
@@ -4,6 +4,7 @@ import { LoaderService, AlertService } from '@capillarytech/pwa-ui-helpers';
 import { TranslateService } from '@ngx-translate/core';
 import { Utils } from '../../../helpers/utils';
 import { BasePage } from '../../../base/base-page';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dealer-selection',
@@ -16,29 +17,30 @@ import { BasePage } from '../../../base/base-page';
 
 export class DealerSelectionPage extends BasePage implements OnInit {
 
-  asSoonPossible: boolean = true;
+  asSoonPossible: boolean = false;
   slotSelected: boolean = false;
   slotContent: string = "";
-  activeTimeSlot : number;
-  constructor(private loaderService: LoaderService, private alertService: AlertService, private translate: TranslateService) {
+  activeTimeSlot: number;
+
+  constructor(private loaderService: LoaderService, private alertService: AlertService, private translate: TranslateService, public modalController: ModalController) {
     super();
 
     // this.loaderService.startLoading();
-    this.slotSelected = this.asSoonPossible;
+    this.slotSelected = true;
     this.slotContent = this.asSoonPossible ? "ASAP" : "";
-    this.activeTimeSlot = this.asSoonPossible ? 0 : null;
+    this.activeTimeSlot = 0;
     this.translate.use(Utils.getLanguageCode());
   }
 
   ngOnInit() {
   }
 
-  // toggleCheckbox() {
-  //   this.asSoonPossible = !this.asSoonPossible;
-  //   this.slotSelected = this.asSoonPossible;
-  //   this.slotContent = this.asSoonPossible ? "ASAP" : "";
-  //   this.activeTimeSlot = this.asSoonPossible ? 0 : null;
-  // }
+  toggleCheckbox() {
+    this.asSoonPossible = !this.asSoonPossible;
+    this.slotSelected = this.asSoonPossible;
+    this.slotContent = this.asSoonPossible ? "ASAP" : "";
+    this.activeTimeSlot = this.asSoonPossible ? 0 : null;
+  }
 
   selectTime(time, index) {
     this.asSoonPossible = (time == 'ASAP') ? true : false;
@@ -53,6 +55,11 @@ export class DealerSelectionPage extends BasePage implements OnInit {
 
   widgetLoadingSuccess(name, data) {
     console.log('loaded ' + name, data);
+    switch (name) {
+      case 'DELIVERYSLOTS':
+        this.asSoonPossible = data[0].id == -1 ? true : false;
+        this.slotContent = this.asSoonPossible ? "ASAP" : data[0].time;
+    }
   }
 
   widgetLoadingFailed(name, data) {
@@ -61,6 +68,14 @@ export class DealerSelectionPage extends BasePage implements OnInit {
 
   widgetActionFailure(name, data) {
     console.log('action failed ' + name, data);
+  }
+
+  closeModal() {
+    this.modalController.dismiss();
+  }
+
+  selectTimeSlot() {
+    this.closeModal();
   }
 
 }
