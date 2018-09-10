7503cae824681e201257884e01eb424d9d8c1e08
Vivek Vaibhaw Dwivedi <vivek.dwivedi@capillarytech.com>
2018-09-10 16:37:00 +0530

    foundation for cart page

--- client/src/app/pages/checkout/cart/cart.module.ts
+++ client/src/app/pages/checkout/cart/cart.module.ts
@@ -2,11 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-import { CartWidgetModule } from '@capillarytech/pwa-framework';
-
+import { CartWidgetModule, VoucherWidgetModule, ImagePreloadModule } from '@capillarytech/pwa-framework';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpLoaderFactory } from '../../../translation.loader';
 import { CartPage } from './cart.page';
+import { AlertServiceModule, AlertService, LoaderServiceModule, LoaderService, DebounceClickDirective } from '@capillarytech/pwa-ui-helpers';
 
 const routes: Routes = [
   {
@@ -21,9 +23,22 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     CartWidgetModule,
+    VoucherWidgetModule,
+    ImagePreloadModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     RouterModule.forChild(routes)
   ],
   declarations: [CartPage],
+  providers: [
+    AlertService,
+    LoaderService,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CartPageModule {}
