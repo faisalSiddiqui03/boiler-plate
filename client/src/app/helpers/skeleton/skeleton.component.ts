c5c43673abba7f5f4fdb37d6e7915490837ece74
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-09-11 13:13:31 +0530

    generalized skeleton component

--- client/src/app/helpers/skeleton/skeleton.component.ts
+++ client/src/app/helpers/skeleton/skeleton.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton',
-  templateUrl: './skeleton.component.html',
+  template: `
+    <div #skeleton class="skeleton-warpper"></div>
+  `,
   styleUrls: ['./skeleton.component.scss']
 })
 export class SkeletonComponent implements OnInit {
 
+  @Input() aspectRatio: number;
+  @Input() width = '';
+  @Input() height = '';
+
+  @ViewChild('skeleton') el: ElementRef;
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+
+    if (this.el.nativeElement) {
+      if (this.aspectRatio) {
+        const paddingTop = (100 / this.aspectRatio).toFixed(2) + '%';
+        this.el.nativeElement.style['padding-top'] = paddingTop;
+      }
+      if (this.width) {
+        this.el.nativeElement.style['width'] = this.width;
+      }
+      if (this.height) {
+        this.el.nativeElement.style['height'] = this.height;
+      }
+    }
+  }
+
 
 }
