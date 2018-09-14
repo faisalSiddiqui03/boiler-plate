a199ebf1858e6b39eb53dde61aa340b666500155
anjicap <anjaneyulu.singavaram@capillarytech.com>
2018-09-14 12:47:47 +0530

    nutrition page ui

--- client/src/app/pages/static/feedback/feedback.page.ts
+++ client/src/app/pages/static/feedback/feedback.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Utils } from '../../../helpers/utils';
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.page.scss'],
 })
 export class FeedbackPage implements OnInit {
+  feedBackForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.translate.use(Utils.getLanguageCode());
+
+    this.feedBackForm = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      mobile: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(8)])],
+      orderNumber: ['', Validators.compose([Validators.required])],
+      orderType: ['', Validators.compose([Validators.required])],
+      message: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  titleValue = '';
 
   ngOnInit() {
+    this.translate.get('feedback_page.feedback').subscribe(value => {
+      this.titleValue = value;
+    });
+  }
+
+  goToPage(pageName) {
+    this.router.navigateByUrl(pageName);
   }
 
 }
