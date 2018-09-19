acadabdda38981f2828013d3810fd73ac8d10b39
Utkarsha Kurekar <utkarsha.kurekar@capillarytech.com>
2018-09-19 16:31:24 +0530

    login page complete. sign up page in progress

--- client/src/app/pages/user/auth/signup/signup.page.ts
+++ client/src/app/pages/user/auth/signup/signup.page.ts
@@ -1,8 +1,22 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { pwaLifeCycle, pageView, OnWidgetActionsLifecyle, OnWidgetLifecyle } from '@capillarytech/pwa-framework';
+import { 
+    Component, 
+    OnInit, 
+    EventEmitter } from '@angular/core';
+import { 
+    Validators, 
+    FormBuilder, 
+    FormGroup, 
+    AbstractControl } from '@angular/forms';
+import { 
+    pwaLifeCycle, 
+    pageView, 
+    OnWidgetActionsLifecyle, 
+    OnWidgetLifecyle } from '@capillarytech/pwa-framework';
 import { BaseComponent } from '../../../../base/base-component';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Utils } from '../../../../helpers/utils';
+
 
 @Component({
   selector: 'app-signup',
@@ -17,16 +31,23 @@ export class SignupPage extends BaseComponent implements OnInit, OnWidgetLifecyl
 
   signUpForm:FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(
+      private formBuilder: FormBuilder, 
+      private router: Router, 
+      private translate: TranslateService
+  ) {
     super();
+    this.translate.use(Utils.getLanguageCode());
     this.signUpForm = this.formBuilder.group({
-      fname: ['', Validators.required],
+      fname: ['', Validators.required, Validators.pattern('^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z-_ \.]*$')],
       lname: [''],
-      mobile: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+      mobile: ['', Validators.required, Validators.pattern('^[2569][0-9]*$'), Validators.minLength(8), Validators.maxLength(8)],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required, Validators.minLength(6)],
       confirmPassword: ['', Validators.required]
     });
+
+    // this.signUpForm.validator = this.matchingPasswords
    }
 
   ngOnInit() {
@@ -54,5 +75,31 @@ export class SignupPage extends BaseComponent implements OnInit, OnWidgetLifecyl
 
   widgetLoadingSuccess(name: string, data: any): any {
   }
+
+  private mapValidators(validators) {
+    const formValidators = [];
+  
+    if(validators) {
+      for(const validation of Object.keys(validators)) {
+          if(validators[validation] === true){
+            formValidators.push(Validators[validation]);
+          }else{
+            formValidators.push(Validators[validation](validators[validation]));
+          }          
+      }
+    }
+  
+    return formValidators;
+  }
+
+    matchingPasswords(AC: AbstractControl) {
+        let password = AC.get('password').value; // to get value in input tag
+        let confirmPassword = AC.get('confirmpassword').value; // to get value in input tag
+        if (password != confirmPassword) {
+            AC.get('confirmpassword').setErrors({ matchingPasswords: true })
+        } else {
+            return null
+        }
+    }
 
 }
