b8e44e0849c0a42f211efbb4e74f0a3d21b39121
anjicap <anjaneyulu.singavaram@capillarytech.com>
2018-09-19 10:43:12 +0530

    checkout page created

---
+++ client/src/app/pages/checkout/checkout/checkout.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CheckoutPage } from './checkout.page';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../../../translation.loader';
+import { HttpClient } from '@angular/common/http';
+import { HeaderModule } from '../../../components/header/header.module';
+import { SubHeaderModule } from '../../../components/sub-header/sub-header.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CheckoutPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    HeaderModule,
+    SubHeaderModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  declarations: [CheckoutPage]
+})
+export class CheckoutPageModule {}
--- client/src/app/pages/user/profile/add-address/add-address.page.ts
+++ client/src/app/pages/user/profile/add-address/add-address.page.ts
@@ -6,9 +6,7 @@ import { Router } from '@angular/router';
 import { LoaderService, AlertService } from '@capillarytech/pwa-ui-helpers';
 import { TranslateService } from '@ngx-translate/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AgmCoreModule, MouseEvent } from '@agm/core';
 import { ModalController } from '@ionic/angular';
-import { SeacrhLocationPage } from '../seacrh-location/seacrh-location.page';
 
 @Component({
   selector: 'app-add-address',
@@ -67,15 +65,3 @@ export class AddAddressPage extends BaseComponent implements OnInit, OnWidgetLif
   }
 
 }
-
-
-
-
-// Interface for type safety.
-interface Marker {
-  lat: number;
-  lng: number;
-  label?: string;
-  draggable: boolean;
-}
-
