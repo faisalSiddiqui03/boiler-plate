5f272383f4c0fc153dccf1050f517292e7d16313
Vivek Vaibhaw Dwivedi <vivek.dwivedi@capillarytech.com>
2018-10-05 15:11:55 +0530

    search should not be case sensitive

--- client/src/app/pages/home/home.page.ts
+++ client/src/app/pages/home/home.page.ts
@@ -80,7 +80,6 @@ export class HomePage extends BaseComponent implements OnInit, OnWidgetLifecyle,
   }
 
   ngOnInit() {
-    console.log('------>', this.getCurrentStore());
   }
 
   ngOnDestroy() {
@@ -374,8 +373,8 @@ export class HomePage extends BaseComponent implements OnInit, OnWidgetLifecyle,
   }
 
   filterEntires(cityList, searchTerm) {
-    const searchSubString = this.isCleared ? '' : searchTerm;
-    return cityList.filter(city => (city.name || '').includes(searchSubString) && city.name);
+    const searchSubString = this.isCleared ? '' : searchTerm.toLowerCase();
+    return cityList.filter(city => (city.name.toLowerCase() || '').includes(searchSubString) && city.name);
   }
 
   getDeliveryMode() {
