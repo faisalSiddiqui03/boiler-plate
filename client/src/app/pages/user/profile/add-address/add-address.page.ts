8e2ed1298e5c0f068a73548a596063076971c2ea
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-10-08 16:36:09 +0530

    update address functino added

--- client/src/app/pages/user/profile/add-address/add-address.page.ts
+++ client/src/app/pages/user/profile/add-address/add-address.page.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { BaseComponent } from '../../../../base/base-component';
-import { pwaLifeCycle, pageView, OnWidgetActionsLifecyle, OnWidgetLifecyle, UserAddressWidgetActions } from '@capillarytech/pwa-framework';
+import {
+  pwaLifeCycle,
+  pageView,
+  OnWidgetActionsLifecyle,
+  OnWidgetLifecyle,
+  UserAddressWidgetActions
+} from '@capillarytech/pwa-framework';
 import { UtilService } from '../../../../helpers/utils';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { LoaderService, AlertService } from '@capillarytech/pwa-ui-helpers';
 import { TranslateService } from '@ngx-translate/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -21,12 +27,20 @@ import { SearchLocationPage } from '../search-location/search-location.page';
 export class AddAddressPage extends BaseComponent implements OnInit, OnWidgetLifecyle, OnWidgetActionsLifecyle {
 
 
-  titleValue: string = '';
+  titleValue = '';
   addAddressForm: FormGroup;
+  addressId;
+  singleUserAddressWidgetActions = new EventEmitter();
 
   constructor(private router: Router,
     private utilService: UtilService,
-    private loaderService: LoaderService, private alertService: AlertService, private translate: TranslateService, private formBuilder: FormBuilder, private modalController: ModalController) {
+    private loaderService: LoaderService,
+    private alertService: AlertService,
+    private translate: TranslateService,
+    private formBuilder: FormBuilder,
+    private modalController: ModalController,
+    private actRoute: ActivatedRoute
+  ) {
     super();
 
     // this.loaderService.startLoading(null, this.getFulfilmentMode().mode === 'H' ? 'delivery-loader': 'pickup-loader');
@@ -41,6 +55,8 @@ export class AddAddressPage extends BaseComponent implements OnInit, OnWidgetLif
   }
 
   ngOnInit() {
+    this.addressId = this.actRoute.snapshot.params['addressId'];
+
     this.translate.get('add_address_page.add_address').subscribe(value => {
       this.titleValue = value;
     });
@@ -57,6 +73,11 @@ export class AddAddressPage extends BaseComponent implements OnInit, OnWidgetLif
 
   widgetLoadingFailed(name: string, data: any): any {
     console.log(name, 'Loading Failed');
+    switch (name) {
+      case 'singleUserAddress':
+        console.log(name, 'error', data);
+        break;
+    }
   }
 
   widgetLoadingStarted(name: string, data: any): any {
@@ -65,6 +86,10 @@ export class AddAddressPage extends BaseComponent implements OnInit, OnWidgetLif
 
   widgetLoadingSuccess(name: string, data: any): any {
     console.log(name, 'Loading Success');
+    switch (name) {
+      case 'singleUserAddress':
+        break;
+    }
   }
 
 }
