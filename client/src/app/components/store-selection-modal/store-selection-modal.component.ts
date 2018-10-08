ce217e81b83e5178c1bc329d948d0a3909cd515c
Vivek Vaibhaw Dwivedi <vivek.dwivedi@capillarytech.com>
2018-10-08 22:50:29 +0530

    wiop

---
+++ client/src/app/components/store-selection-modal/store-selection-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-selection-modal',
+  templateUrl: './store-selection-modal.component.html',
+  styleUrls: ['./store-selection-modal.component.scss']
+})
+export class StoreSelectionModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
