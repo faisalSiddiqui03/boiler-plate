cae3089460c3c4917e2d85b8eb2031ee475414c6
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-10-10 18:02:57 +0530

    success page updated

--- client/src/app/pages/checkout/success/success.page.ts
+++ client/src/app/pages/checkout/success/success.page.ts
@@ -32,25 +32,47 @@ export class SuccessPage extends BaseComponent implements OnInit {
   orderId: number;
   email: string;
   orderDetailWidgetAction = new EventEmitter();
+  showEmailPopup = false;
+  inputEmail = '';
+  isLoadingFailed = false;
 
   ngOnInit() {
     this.translate.use(this.getCurrentLanguageCode());
-
+    this.isLoadingFailed = false;
     this.orderId = this.route.snapshot.params.orderId;
-    this.email = atob(this.route.snapshot.params.email);
+    const emailInput = this.route.snapshot.params.email;
+    if (emailInput) {
+      this.email = atob(this.route.snapshot.params.email);
+    } else {
+      this.showEmailPopup = true;
+    }
+  }
+
+  getOrderUsingEmail() {
+    this.showEmailPopup = false;
+    this.email = this.inputEmail;
+    this.router.navigateByUrl(
+      this.getNavigationUrlWithLangSupport(
+        'success/' + this.orderId + '/' + btoa(this.email)
+    ));
   }
 
   goToPage(pageName) {
+    if (pageName === 'product') {
+      this.capRouter.routeByUrlWithLanguage('/products?category=deals&id=CU00215646');
+    }
     this.capRouter.routeByUrlWithLanguage(pageName);
     // this.router.navigateByUrl(this.getNavigationUrlWithLangSupport(pageName));
   }
 
   widgetActionFailed(name: string, data: any): any {
     console.log(name, 'Action Failed');
+    this.isLoadingFailed = true;
   }
 
   widgetActionSuccess(name: string, data: any): any {
     console.log(name, 'Action Success');
+    this.isLoadingFailed = false;
   }
 
   widgetLoadingFailed(name: string, data: any): any {
