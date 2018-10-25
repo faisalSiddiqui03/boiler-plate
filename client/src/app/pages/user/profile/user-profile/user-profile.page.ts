58dde94878f4491ccb55bf74721a2bcc2aaf5a19
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-10-26 02:25:33 +0530

    three comps added

--- client/src/app/pages/user/profile/user-profile/user-profile.page.ts
+++ client/src/app/pages/user/profile/user-profile/user-profile.page.ts
@@ -1,16 +1,13 @@
-import { Component, EventEmitter } from '@angular/core';
-import { BaseComponent } from '@capillarytech/pwa-components/base-component';
+import { Component } from '@angular/core';
 import {
   pwaLifeCycle,
   pageView,
-  Action,
-  OnWidgetActionsLifecyle, OnWidgetLifecyle, CapRouterService
+  CapRouterService
 } from '@capillarytech/pwa-framework';
-import { UserProfileWidgetActions } from '@cap-widget/user-profile';
-import { Router } from '@angular/router';
 import { LoaderService, AlertService } from '@capillarytech/pwa-ui-helpers';
 import { TranslateService } from '@ngx-translate/core';
-import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserProfileComponent } from '@capillarytech/pwa-components/user-profile/user-profile.component'
 
 @Component({
   selector: 'app-user-profile',
@@ -20,23 +17,20 @@ import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @pwaLifeCycle()
 @pageView()
-
-export class UserProfilePage extends BaseComponent implements OnWidgetLifecyle, OnWidgetActionsLifecyle {
+export class UserProfilePage extends UserProfileComponent {
   
-  updateProfileActionEmitter = new EventEmitter();
   profileForm: FormGroup;
-  widgetModel: any;
   loaded: boolean;
   updateInProgress = false;
 
-  constructor(private router: Router,
+  constructor(
     private loaderService: LoaderService,
     private alertService: AlertService,
     private translate: TranslateService,
     private formBuilder: FormBuilder,
-    private capRouter: CapRouterService,) {
+    private capRouter: CapRouterService
+  ) {
     super();
-    this.translate.use(this.getCurrentLanguageCode());
 
     this.profileForm = this.formBuilder.group({
       firstName: ['', Validators.compose([Validators.required])],
@@ -52,43 +46,27 @@ export class UserProfilePage extends BaseComponent implements OnWidgetLifecyle, 
     this.widgetModel.firstName = this.profileForm.value.firstName;
     this.widgetModel.lastName = this.profileForm.value.lastName;
     this.widgetModel.mobileNo = this.profileForm.value.mobileNo;
-    this.updateProfileActionEmitter.emit(new Action(UserProfileWidgetActions.ACTION_UPDATE_USER));
+    this.updateUserProfile();
   }
 
-  async widgetActionSuccess(name, data) {
-    console.log('action success ' + name, data);
+  async handleUpdateUserActionLogoutSuccess(data) {
     this.loaderService.stopLoading();
     this.updateInProgress = false;
     await this.alertService.presentToast(this.translate.instant('user_profile_page.profile_updated_successfully'), 2000, 'top', 'top');
     this.capRouter.routeByUrl('my-account');
-    // this.router.navigateByUrl(this.getNavigationUrlWithLangSupport('my-account'));
   }
 
-  widgetLoadingSuccess(name, data) {
-    switch (name) {
-      case 'USER_PROFILE':
-        this.widgetModel = data;
-        this.loaded = true;
-        console.log('loaded ' + name, data);
-        this.profileForm.get('firstName').setValue(data.firstName);
-        this.profileForm.get('lastName').setValue(data.lastName);
-        this.profileForm.get('mobileNo').setValue(data.mobileNo);
-        this.profileForm.get('alternateEmail').setValue(data.alternateEmail);
-        break;
-    }
+  handleUserProfileLoadingSuccess(data) {
+    this.loaded = true;
+    this.profileForm.get('firstName').setValue(data.firstName);
+    this.profileForm.get('lastName').setValue(data.lastName);
+    this.profileForm.get('mobileNo').setValue(data.mobileNo);
+    this.profileForm.get('alternateEmail').setValue(data.alternateEmail);
   }
 
-  widgetLoadingFailed(name, data) {
-    console.log('loading failed' + name, data);
-  }
-
-  async widgetActionFailed(name: string, data: any) {
+  async handleUpdateUserActionLogoutFailed(data) {
     this.updateInProgress = false;
     this.loaderService.stopLoading();
     await this.alertService.presentToast(this.translate.instant('user_profile_page.profile_update_failed'), 2000, 'top', 'top');
   }
-
-  widgetLoadingStarted(name: string, data: any): any {
-  }
-
 }
