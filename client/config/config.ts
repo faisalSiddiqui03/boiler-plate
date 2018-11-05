9d4563331296debc3b6add2971cb1f64b00f91e0
Prakhar Verma <prakhar.verma@capillarytech.com>
2018-11-05 16:38:32 +0530

    sub header changes

--- client/config/config.ts
+++ client/config/config.ts
@@ -11,7 +11,7 @@ export const config = {
   'headerBannerRefCode': 'ph-60th-years-',
   'footerBannerRefCode': 'bogo-pwa-',
   'quantityEnabledCategories': ['CU00215602'],
-  'baseUrl': '/api/',
+	'baseUrl': '/api/',
   'tracker': {
     'widgetImpression': false,
     'apiTrack': false
